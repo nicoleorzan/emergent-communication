17caf9dee5694a097058710122cc0a3a225fa596
Nicole <orzan.nicole@gmail.com>
2021-04-14 17:11:40 +0200

    added plot

--- replicator_dynamics.py
+++ replicator_dynamics.py
@@ -6,8 +6,12 @@ rep = Replicator1D()
 
 max_time = 100
 time = np.arange(0, max_time, .1)
-y0 = [0.1, 0.8, 0.1]
 
-y = rep.loop_ode(y0, time)
-plt.plot(y[:,0], y[:,1])
+space = np.linspace(0.05, 0.95, 10)
+for y_0 in space:
+    for y_1 in np.linspace(0.05, 0.95-y_0, 10):
+        y0 = [y_0, y_1, 1.-(y_0 + y_1)]
+        print("y0=", y0)
+        y = rep.loop_ode(y0, time)
+        plt.plot(y[:,0], y[:,1])
 plt.show()
